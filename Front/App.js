c493d65ee07f035f0a01e9f588ba7317ba910546
SongDaeSeon <1561eotjs@naver.com>
2024-06-26 09:36:59 +0900

    Feat MAIN02_CALENDAR 경로 수정

--- Front/App.js
+++ Front/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { FoodProvider } from './FoodContext'; // FoodProvider 임포트
 import CalendarScreen from './CalendarScreen';
 import Cal from './Cal';
 import Add_Text from './Add_Text';
@@ -13,36 +14,38 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Calendar">
-          <Stack.Screen 
-            name="Calendar" 
-            component={CalendarScreen} 
-            options={{ headerShown: false }} 
-          />
-          <Stack.Screen 
-            name="Cal" 
-            component={Cal} 
-            options={{ headerShown: false }} 
-          />
-          <Stack.Screen 
-            name="AddText" 
-            component={Add_Text} 
-            options={{ headerShown: false }} 
-          />
-          <Stack.Screen 
-            name="FoodList" 
-            component={FoodList} 
-            options={{ headerShown: false }} 
-          />
-          <Stack.Screen 
-            name="FoodListModify" 
-            component={FoodListModify} 
-            options={{ headerShown: false }} 
-          />
-          {/* Add any additional screens here */}
-        </Stack.Navigator>
-      </NavigationContainer>
+      <FoodProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Calendar">
+            <Stack.Screen 
+              name="Calendar" 
+              component={CalendarScreen} 
+              options={{ headerShown: false }} 
+            />
+            <Stack.Screen 
+              name="Cal" 
+              component={Cal} 
+              options={{ headerShown: false }} 
+            />
+            <Stack.Screen 
+              name="AddText" 
+              component={Add_Text} 
+              options={{ headerShown: false }} 
+            />
+            <Stack.Screen 
+              name="FoodList" 
+              component={FoodList} 
+              options={{ headerShown: false }} 
+            />
+            <Stack.Screen 
+              name="FoodListModify" 
+              component={FoodListModify} 
+              options={{ headerShown: false }} 
+            />
+            {/* Add any additional screens here */}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FoodProvider>
     </SafeAreaProvider>
   );
 };
