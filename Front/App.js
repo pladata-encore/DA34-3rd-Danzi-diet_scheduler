3887046afebcb37fa2e5d33c96ace19ee79df155
Kimyu-ri <161406927+Kimyu-ri@users.noreply.github.com>
2024-06-26 09:48:50 +0900

    Feat API 신규추가
    회원가입/로그인 서버 연결, 서버 주소 config 파일 생성

--- Front/App.js
+++ Front/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+// import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import LoadingScreen from './loading'; 
 import Login_typing from './Login_typing'; 
 import SignupForm from './SignupForm'; 
@@ -28,19 +28,17 @@ export default function App() {
   }
 
   return (
-
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Signup" component={SignupForm} />
-            <Stack.Screen name="Login" component={Login_typing} />
-            <Stack.Screen name="DietInfo" component={DietInfo} />
-            <Stack.Screen name="MainPage" component={MainPage} />
-            <Stack.Screen name="MyPage" component={MyPage} /> 
-            <Stack.Screen name="Modify" component={Modify} /> 
-            <Stack.Screen name="MyModify" component={MyModify} /> 
-            <Stack.Screen name="Insight" component={Insight} /> 
-          </Stack.Navigator> 
-        </NavigationContainer>
-
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Signup" component={SignupForm} />
+        <Stack.Screen name="Login" component={Login_typing} />
+        <Stack.Screen name="DietInfo" component={DietInfo} />
+        <Stack.Screen name="MainPage" component={MainPage} />
+        <Stack.Screen name="MyPage" component={MyPage} /> 
+        <Stack.Screen name="Modify" component={Modify} /> 
+        <Stack.Screen name="MyModify" component={MyModify} /> 
+        <Stack.Screen name="Insight" component={Insight} /> 
+      </Stack.Navigator> 
+    </NavigationContainer>
   );
 }
---
+++ Front/config.js
@@ -0,0 +1,5 @@
+const config = {
+    BASE_URL : 'http://ec2-43-203-68-109.ap-northeast-2.compute.amazonaws.com:8000'
+};
+
+module.exports = config;
